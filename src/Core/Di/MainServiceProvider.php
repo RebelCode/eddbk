eb43ab632776eeeed6525e2db26617bacd2d4f12
Miguel Muscat <miguelmuscat93@gmail.com>
2017-06-26 14:59:43 +0200

    Added `MainServiceProvider`

---
+++ src/Core/Di/MainServiceProvider.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace RebelCode\EddBookings\Core\Di;
+
+use Dhii\Machine\LoopMachine;
+use Interop\Container\ContainerInterface;
+use RebelCode\EddBookings\Core\I18n\WpTranslator;
+
+/**
+ * The main service provider.
+ *
+ * @since[*next-version*]
+ */
+class MainServiceProvider extends AbstractBaseServiceProvider
+{
+    /**
+     * The service ID of the factory.
+     *
+     * @since [*next-version*]
+     */
+    const SID_FACTORY = 'factory';
+
+    /**
+     * The service ID of the loop machine.
+     *
+     * @since [*next-version*]
+     */
+    const SID_LOOP_MACHINE = 'loop_machine';
+
+    /**
+     * The service ID of the I18n component.
+     *
+     * @since [*next-version*]
+     */
+    const SID_I18N = 'i18n';
+
+    /**
+     * {@inheritdoc}
+     *
+     * @since [*next-version*]
+     */
+    protected function _getServices()
+    {
+        return $this->_prepare(array(
+            static::SID_FACTORY      => 'getFactory',
+            static::SID_LOOP_MACHINE => 'getLoopMachine',
+            static::SID_I18N         => 'getI18n',
+        ));
+    }
+
+    /**
+     * Service definition for a generic factory.
+     *
+     * @since[*next-version*]
+     *
+     * @param ContainerInterface $c        The container instance.
+     * @param mixed              $previous The previous instance, if any. Default: null
+     * @param array              $config   Any configuration data. Default: array()
+     *
+     * @return ContainerInterface
+     */
+    public function getFactory(ContainerInterface $c, $previous = null, array $config = array())
+    {
+        return $c;
+    }
+
+    /**
+     * Service definition for a loop machine.
+     *
+     * @since[*next-version*]
+     *
+     * @param ContainerInterface $c        The container instance.
+     * @param mixed              $previous The previous instance, if any. Default: null
+     * @param array              $config   Any configuration data. Default: array()
+     *
+     * @return LoopMachine
+     */
+    public function getLoopMachine(ContainerInterface $c, $previous = null, array $config = array())
+    {
+        return new LoopMachine();
+    }
+
+    /**
+     * Service definition for an internationalization translator.
+     *
+     * @since[*next-version*]
+     *
+     * @param ContainerInterface $c        The container instance.
+     * @param mixed              $previous The previous instance, if any. Default: null
+     * @param array              $config   Any configuration data. Default: array()
+     *
+     * @return WpTranslator
+     */
+    public function getI18n(ContainerInterface $c, $previous = null, array $config = array())
+    {
+        return new WpTranslator(EDDBK_TEXT_DOMAIN);
+    }
+}
